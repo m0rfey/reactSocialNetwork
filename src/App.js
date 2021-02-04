ce933018d72268a3bb08632a7d1cdf99e0093860
m0rfey <maksimchuk.mm@gmail.com>
2021-02-04 13:16:39 +0200

    dialogs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 import Footer from "./components/Footer/Footer";
+import Dialogs from "./components/Dialogs/Dialogs"
 import style from './App.module.css';
 
 class App extends Component {
@@ -11,7 +12,10 @@ class App extends Component {
             <div className={style.wrapper}>
                 <Header/>
                 <Navbar/>
-                <Profile/>
+                <div className={style.content}>
+                    <Profile/>
+                    {/*<Dialogs />*/}
+                </div>
                 <Footer/>
             </div>
         );
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import style from './Dialogs.module.css'
+
+class Dialogs extends Component {
+    render() {
+        return(
+            <h2>Messages</h2>
+        )
+    }
+}
+
+export default Dialogs;
